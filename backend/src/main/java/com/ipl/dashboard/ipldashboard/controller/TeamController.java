ad4a07896d3638e5483c84bed3f59e99eabec402
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2024-03-21 12:09:59 +0530

    implemented sorting and pagination to return the 4 recent matches played by a team

--- backend/src/main/java/com/ipl/dashboard/ipldashboard/controller/TeamController.java
+++ backend/src/main/java/com/ipl/dashboard/ipldashboard/controller/TeamController.java
@@ -3,6 +3,8 @@ package com.ipl.dashboard.ipldashboard.controller;
 import com.ipl.dashboard.ipldashboard.model.Team;
 import com.ipl.dashboard.ipldashboard.repository.MatchRepository;
 import com.ipl.dashboard.ipldashboard.repository.TeamRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +22,12 @@ public class TeamController {
 
     @GetMapping("/team/{teamName}")
     public Team getTeam(@PathVariable String teamName) {
-        return teamRepository.findByTeamName(teamName);
+        Team team = teamRepository.findByTeamName(teamName);
+        Pageable pageable = PageRequest.of(0, 4);
+        team.setMatches(matchRepository.getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, pageable));
+
+        return team;
     }
+
 
 }
--- backend/src/main/java/com/ipl/dashboard/ipldashboard/repository/MatchRepository.java
+++ backend/src/main/java/com/ipl/dashboard/ipldashboard/repository/MatchRepository.java
@@ -1,11 +1,12 @@
 package com.ipl.dashboard.ipldashboard.repository;
 
 import com.ipl.dashboard.ipldashboard.model.Match;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface MatchRepository extends CrudRepository<Match, Long> {
 
-    List<Match> getByTeam1orTeam2(String teamName1, String teamName2);
+    List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2, Pageable pageable);
 }
