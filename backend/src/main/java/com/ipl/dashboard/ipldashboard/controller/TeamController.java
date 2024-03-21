66df8b399f5b1e811175878030c75e1998fdcb56
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2024-03-21 12:37:17 +0530

    refactor: move pagination to MatchRepository from TeamController

--- backend/src/main/java/com/ipl/dashboard/ipldashboard/controller/TeamController.java
+++ backend/src/main/java/com/ipl/dashboard/ipldashboard/controller/TeamController.java
@@ -3,8 +3,6 @@ package com.ipl.dashboard.ipldashboard.controller;
 import com.ipl.dashboard.ipldashboard.model.Team;
 import com.ipl.dashboard.ipldashboard.repository.MatchRepository;
 import com.ipl.dashboard.ipldashboard.repository.TeamRepository;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +21,7 @@ public class TeamController {
     @GetMapping("/team/{teamName}")
     public Team getTeam(@PathVariable String teamName) {
         Team team = teamRepository.findByTeamName(teamName);
-        Pageable pageable = PageRequest.of(0, 4);
-        team.setMatches(matchRepository.getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, pageable));
+        team.setMatches(matchRepository.findLatestMatchesByTeam(teamName, 4));
 
         return team;
     }
