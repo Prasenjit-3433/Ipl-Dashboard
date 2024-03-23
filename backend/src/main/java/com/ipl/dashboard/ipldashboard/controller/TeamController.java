cefec7a9d2601b1e74a808cf56f5b6ab8d0a47d3
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2024-03-23 12:47:14 +0530

    fething and rendering latest 4 matches

--- backend/src/main/java/com/ipl/dashboard/ipldashboard/controller/TeamController.java
+++ backend/src/main/java/com/ipl/dashboard/ipldashboard/controller/TeamController.java
@@ -3,11 +3,13 @@ package com.ipl.dashboard.ipldashboard.controller;
 import com.ipl.dashboard.ipldashboard.model.Team;
 import com.ipl.dashboard.ipldashboard.repository.MatchRepository;
 import com.ipl.dashboard.ipldashboard.repository.TeamRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private TeamRepository teamRepository;
