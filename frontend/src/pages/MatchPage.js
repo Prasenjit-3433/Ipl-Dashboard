4a5b1e5c8ea105f1113f62291cbe886770a7e8f7
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2024-03-24 14:58:24 +0530

    added match page that shows matches by year for a team

--- frontend/src/pages/MatchPage.js
+++ frontend/src/pages/MatchPage.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import MatchDetailCard from "../components/MatchDetailCard";
+import { useParams } from "react-router-dom";
+
 export const MatchPage = () => {
+    const [matches, setMatches] = useState([]);
+    const {teamName, year} = useParams();
+
+    useEffect(() => {
+        const fetchMatches = async () => {
+            const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+            const data = await response.json();
+
+            setMatches(data);
+        }
+
+        fetchMatches();
+    }, [teamName, year]);
+
     return(
         <div className="match-page">
             <h1>Match Page</h1>
+
+            {
+                matches && matches.map(match => <MatchDetailCard key={match.id} teamName={teamName} match={match}/>)
+            }
         </div>
     );
 }
